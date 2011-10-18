f03c04c31c32d01de1efd1107f6c491de8c5db71
Yuki Nishijima <mail@yukinishijima.net>
2011-10-18 22:15:39 +0900

    show loading dialog when translating

--- assets/www/google_translator.jquery.js
+++ assets/www/google_translator.jquery.js
@@ -2,12 +2,14 @@
 var result = "";
 
 function translate(text, from, to){
+  $.mobile.showPageLoadingMsg();
   text = encodeURIComponent((text || $('#text_to_translate').val()));
   from = encodeURIComponent((from || $("#from_lang").val()));
   to = encodeURIComponent((to || $("#to_lang").val()));
   $.getJSON("https://ajax.googleapis.com/ajax/services/language/translate", "v=1.0&q="+text+"&langpair="+from+"%7C"+to+"&callback=?", function(data){
     result = data.responseData.translatedText;
     $("#translated_text").html(result);
+    $.mobile.hidePageLoadingMsg();
   });
   return result;
 };
