6cd37ca21822b9d5c716fe0cf4995ec880709d77
Yuki Nishijima <mail@yukinishijima.net>
2011-10-19 01:33:10 +0900

    re-implemented translate function.

--- assets/www/google_translator.jquery.js
+++ assets/www/google_translator.jquery.js
@@ -1,26 +1,6 @@
 // this is for testing.
 var result = "";
 
-function translate(text, from, to){
-
-  text = encodeURIComponent((text || $('#text_to_translate').val()));
-  from = encodeURIComponent((from || $("#from_lang").val()));
-  to = encodeURIComponent((to || $("#to_lang").val()));
-
-  if (text == "") {
-    navigator.notification.alert('Please type the text to translate.', null, 'Error', 'close');
-    return null;
-  }
-
-  $.mobile.showPageLoadingMsg();
-  $.getJSON("https://ajax.googleapis.com/ajax/services/language/translate", "v=1.0&q="+text+"&langpair="+from+"%7C"+to+"&callback=?", function(data){
-    result = data.responseData.translatedText;
-    $("#translated_text").html(result);
-    $.mobile.hidePageLoadingMsg();
-  });
-  return result;
-};
-
 function inverseLang(lang){
   return (lang == "ja" ? "en" : "ja");
 };
@@ -31,6 +11,38 @@ function inverseLang(lang){
     this.each(function(){
       $(selector).val(inverseLang($(this).val()));
     });
+
+    return this;
+  };
+
+  jQuery.fn.translate = function(options){
+    var options = jQuery.extend({
+      text: $(this).val(),
+      show: "#translated_text",
+      from: $("#from_lang").val(),
+      to  : $("#to_lang").val()
+    }, options);
+
+    $.mobile.showPageLoadingMsg();
+
+    text = encodeURIComponent(options.text);
+    from = encodeURIComponent(options.from);
+    to = encodeURIComponent(options.to);
+
+    if (text == "") {
+      navigator.notification.alert('Please type the text to translate.',
+                                   $.mobile.hidePageLoadingMsg(),
+                                   'Error', 'close');
+      return null;
+    }
+
+    $.getJSON("https://ajax.googleapis.com/ajax/services/language/translate", "v=1.0&q="+text+"&langpair="+from+"%7C"+to+"&callback=?", function(data){
+      result = data.responseData.translatedText;
+      $(options.show).html(result);
+      $.mobile.hidePageLoadingMsg();
+    });
+
+    // this.each(function(){});
 
     return this;
   };
